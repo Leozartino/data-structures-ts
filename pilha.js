43b701e7e20ddbdfe7c52920a7cba8a320eca242
Leonardo Oliveira <38565099+Leozartino@users.noreply.github.com>
2019-11-21 20:36:14 -0300

    implementação de uma pilha em js

---
+++ pilha.js
@@ -0,0 +1,49 @@
+function Pilha() {
+  this.index = 0;
+  this.storage = [];
+
+  this.insertPilha = value => {
+    this.storage[this.index] = value;
+    this.index += 1;
+  };
+
+  this.removePilha = () => {
+    if (this.index > 0) {
+      this.index -= 1;
+      const result = this.storage[this.index];
+      this.storage.splice(this.index, 1);
+      return result;
+    }
+    return 'Não há itens na pilha';
+  };
+
+  this.sizePilha = () => {
+    return this.index;
+  };
+  this.showPilha = () => {
+    return this.storage;
+  };
+
+  this.peekPilha = () => {
+    return this.storage[this.index - 1];
+  };
+}
+
+const p = new Pilha();
+
+p.insertPilha(5);
+p.insertPilha(7);
+p.insertPilha(8);
+p.insertPilha(9);
+p.insertPilha(7);
+
+console.log(p.showPilha());
+
+p.removePilha();
+p.removePilha();
+
+console.log(p.showPilha());
+
+console.log(p.peekPilha());
+
+console.log(p.sizePilha());
