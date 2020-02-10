a13268c0096188c82c140c400e452e4e8802663c
Leonardo Oliveira <leozartino@gmail.com>
2020-02-10 09:29:40 -0300

    refactoring the queue code

---
+++ queue.js
@@ -0,0 +1,42 @@
+//FIFO
+class Queue {
+  constructor() {
+    this.index = 0;
+    this.lowestIndex = 0;
+    this.queue = {};
+  }
+
+  insertQueue = value => {
+    this.queue[this.index] = value;
+    this.index += 1;
+  };
+  removeQueue = () => {
+    if (!this.isEmpty()) {
+      const result = this.queue[this.lowestIndex];
+      delete this.queue[this.lowestIndex];
+      this.lowestIndex += 1;
+      return result;
+    }
+    return "Error, NOT AUTHORIZED!";
+  };
+  peek = () => (this.isEmpty() ? undefined : this.queue[lowestIndex]);
+
+  size = () => this.index;
+
+  isEmpty = () => this.index - this.lowestIndex === 0;
+
+  showQueue = () => this.queue;
+
+  clearQueue = () => {
+    this.queue = {};
+    this.index = 0;
+    this.lowestIndex = 0;
+  };
+}
+
+const bankQueue = new Queue();
+
+bankQueue.insertQueue("Jon");
+bankQueue.insertQueue("Marie");
+
+console.log(bankQueue.showQueue());
